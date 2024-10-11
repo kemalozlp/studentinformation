faa60bd3cb44a8890fcbbaf30424605e60d908de
kemalozlp <ozlpkemal@gmail.com>
2024-10-11 22:42:54 +0300

    projeye başlandı

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="vercelyukleme">
+      <h1>yeniden baslama</h1>
+    </div>
+  );
+}
